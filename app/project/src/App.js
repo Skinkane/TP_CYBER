103df67642e58a2504697350f9178f875e8f9cb4
Gautier couture <2017566295@lacatholille.fr>
2021-01-02 17:17:46 +0100

    edit project sources

---
+++ app/project/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Info from './Informations.js';
+import MyClock from './Clock.js';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+      <Info/>
+
+
+      </header>
+      <footer>
+      <MyClock/>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/project/src/Informations.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Info extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>Application REACT</h1>
+        </div>);
+    }
+}
